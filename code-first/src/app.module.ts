65ef191140c7982fe266b69cc13f9fa6b79efed5
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-08-22 23:54:52 +0630

    database first approach success

--- code-first/src/app.module.ts
+++ code-first/src/app.module.ts
@@ -8,6 +8,7 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import { BlogsModule } from './blogs/blogs.module';
 import { CommentsModule } from './comments/comments.module';
 import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { join } from 'path';
     }),
     BlogsModule,
     CommentsModule,
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/public?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.2.2'),
   ],
   controllers: [AppController],
   providers: [AppService],
