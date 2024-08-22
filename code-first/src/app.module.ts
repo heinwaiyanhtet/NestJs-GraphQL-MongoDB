ab95f647aafd0885277d2ab28fb65c5c29765f24
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-08-22 23:12:07 +0630

    setup codefirst

---
+++ code-first/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { BlogsModule } from './blogs/blogs.module';
+import { CommentsModule } from './comments/comments.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+    }),
+    BlogsModule,
+    CommentsModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
